589ccee3bdfbe2cc5ee43b40ae5948dfd955f3d6
Jens Caenen <jens.caenen@telenet.be>
2023-10-28 20:53:54 +0200

    Move all 'Cfg' items in description.ext to seperate files + Fix a small issue with 'BattleFieldGridSize' bug + Clean up some code based on conventions

---
+++ functions/functions.hpp
@@ -0,0 +1,13 @@
+class CfgFunctions {
+    class A3C {
+        tag = "A3C";
+        class Game {
+            class createInfantrySquad {};
+            class getPawn {};
+        };
+        class Map {
+            class toggleGridsOnMap {};
+            class drawLineOnMap {};
+        };
+    }
+}
