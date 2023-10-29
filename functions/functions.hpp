d99ee023e73ac6658245f86d734fdc0259ec36a7
HungryDev <robinpeeters1401@gmail.com>
2023-10-29 10:20:55 +0100

    Add function for getting grid lat lon (truncating to always left bottom corner for grid id) and another for getting center pos of grid to spawn resources and objectives on

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -5,6 +5,10 @@ class CfgFunctions {
             class createInfantrySquad {};
             class getPawn {};
         };
+        class Grid {
+            class calculateCenterPosByGridLatLon {};
+            class truncateLatLonToGridLatLon {};
+        };
         class Map {
             class toggleGridsOnMap {};
             class drawLineOnMap {};
